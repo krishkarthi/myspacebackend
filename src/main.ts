6213b641d9230ded836618d30bf7fad87d6e6ccf
Karthikeyan Sekar KV (DHL IT Services) <sekar.karthi80@gmail.com>
2021-09-03 15:56:10 +0530

    cross funtions disabled

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,53 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
+// import { join } from 'path';
+// import Handlebars from 'handlebars';
+
+// async function bootstrap() {
+//   // const app = await NestFactory.create(AppModule);
+
+//   const app = await NestFactory.create<NestFastifyApplication>(
+//     AppModule,
+//     new FastifyAdapter(),
+//   );
+
+//   app.useStaticAssets({
+//     root: join(__dirname, '..', 'public'),
+//     prefix: '/public/',
+//   });
+
+//   app.setViewEngine({
+//     engine: {
+//       handlebars: require('handlebars'),
+//     },
+//     templates: join(__dirname, '..', 'src/attendance/views'),
+//   });
+
+//   Handlebars.registerHelper('loud', function (aString) {
+//     return aString.toUpperCase()
+//   })
+
+//   app.enableCors();
+//   await app.listen(8080);
+//   console.log(`Application is running on: ${await app.getUrl()}`);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import Handlebars from 'handlebars';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-
-  const app = await NestFactory.create<NestFastifyApplication>(
+  const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
-    new FastifyAdapter(),
   );
 
-  app.useStaticAssets({
-    root: join(__dirname, '..', 'public'),
-    prefix: '/public/',
-  });
-
-  app.setViewEngine({
-    engine: {
-      handlebars: require('handlebars'),
-    },
-    templates: join(__dirname, '..', 'src/attendance/views'),
-  });
-
-  Handlebars.registerHelper('loud', function (aString) {
-    return aString.toUpperCase()
-  })
-
-  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src/attendance/views'));
+  app.setViewEngine('hbs');
+    
   await app.listen(8080);
-  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
